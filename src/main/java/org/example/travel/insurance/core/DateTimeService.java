9d0aeda560a1ff7be84cb33ea5969dac6617532b
Anastasia Grabun <anastasiamoon20@mail.ru>
2025-04-23 16:11:53 +0300

    fix working with date

--- src/main/java/org/example/travel/insurance/core/DateTimeService.java
+++ src/main/java/org/example/travel/insurance/core/DateTimeService.java
@@ -2,11 +2,13 @@ package org.example.travel.insurance.core;
 
 import org.example.travel.insurance.rest.TravelCalculatePremiumRequest;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
 import static java.math.BigDecimal.valueOf;
 
 class DateTimeService {
-    public BigDecimal calculateDurationInDays(TravelCalculatePremiumRequest request){
+    long calculateDurationInDays(TravelCalculatePremiumRequest request){
         long differenceInMilliseconds = request.getAgreementDateTo().getTime() - request.getAgreementDateFrom().getTime();
-        return valueOf(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+        return TimeUnit.DAYS.convert(differenceInMilliseconds, TimeUnit.MILLISECONDS);
     }
 }
