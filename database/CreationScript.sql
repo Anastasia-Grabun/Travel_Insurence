17a8d6e372dda15f5ea51dc665f61121ed2e43c4
Anastasia Grabun <anastasiamoon20@mail.ru>
2025-05-29 16:29:49 +0300

    add Entities

--- database/CreationScript.sql
+++ database/CreationScript.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS classifiers (
     id BIGINT PRIMARY KEY,
     title VARCHAR(200) NOT NULL UNIQUE,
     description VARCHAR(100) NOT NULL
-    );
+);
 
 CREATE TABLE IF NOT EXISTS classifier_values (
     id BIGSERIAL PRIMARY KEY,
